d81835e41a6f318694532af4f8532c262550b076
Roman Kozlovskyi <kroman0@users.noreply.github.com>
2015-03-09 14:46:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys, os
 def read(*pathnames):
     return open(os.path.join(os.path.dirname(__file__), *pathnames)).read()
 
-version = '1.0'
+version = '1.1'
 
 setup(name='collective.portlet.carousel',
       version=version,
